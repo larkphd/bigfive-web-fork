0f4846229f48a193f0ca41201e5eb28f68ca6040
larkphd <199968874+larkphd@users.noreply.github.com>
2025-03-03 19:00:17 +0100

    Update page.tsx

--- web/src/app/[locale]/about/page.tsx
+++ web/src/app/[locale]/about/page.tsx
@@ -46,6 +46,30 @@ export default function AboutPage({ params: { locale } }: Props) {
           or social gatherings! Discover yourself and those around you in a whole new way with 
           understandme2.com – where psychology meets engagement and practical use in every setting.
         </p>
+        <p>
+        How Our Platform Helps
+<br>
+Our website is designed to provide valuable personality insights through engaging articles, guides, and exercises. Here’s what you’ll find:<br>
+	•	Comprehensive Articles – Explore how personality impacts different aspects of life, from teamwork and leadership to parenting and mental health.<br
+	•	Personality Tests – Learn more about your own Big Five traits with quick and insightful assessments.<br>
+	•	Practical Applications – Discover how to use personality psychology in education, family dynamics, sports teams, and personal development.<p>
+Who Can Benefit?
+	•	Individuals – Gain self-awareness and strategies for personal growth.
+	•	Parents – Understand your child’s personality and support their mental resilience.
+	•	Educators – Foster a positive classroom environment through personality-based approaches.
+	•	Teams & Leaders – Use personality insights to enhance collaboration and performance.
+        </p>
+        <p>
+          Your Privacy Matters
+          We prioritize privacy and security. Our personality test does not store personal data, does not require login, and does not collect identifying information. 
+          The test is anonymous, ensuring a safe and confidential experience for all users. Just some ads to have the website going.
+
+          <p></p>Join the Journey of Self-Discovery</p>
+
+         Understanding personality is not about labeling—it’s about growth, connection, and unlocking potential. Whether you’re taking the test, reading our articles, 
+         or applying Big Five insights in daily life, we’re here to support your journey toward deeper self-awareness and better relationships.
+         Ready to explore? Take the test, read our insights, and discover the power of personality psychology today!
+        </p>
         <br />
         <p>
           What if... you wonder read the{' '}
